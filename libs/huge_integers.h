746b3caa79ed7dd306c2ea34d7e3796e3174f476
Daniel Sousa <daniel@sousa.me>
2014-04-21 12:24:45 +0100

    Forgot to put huge_product in huge_integers.h

--- libs/huge_integers.h
+++ libs/huge_integers.h
@@ -2,4 +2,5 @@ typedef int* huge_int;
 
 void copy_huge_int(long s, huge_int a, huge_int b); //Takes the integer in a and puts it in b
 void huge_sum(long s, huge_int a, huge_int b, huge_int r);
+void huge_product(long s, huge_int a, huge_int b, huge_int r);
 void print_huge_int(long s, huge_int a);
